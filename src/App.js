517b67e6bf3689bb596e91d403fa307ddbc04e21
Kadir Karabacak <kadirt@hotmail.com>
2023-12-16 17:42:04 +0300

    Project is working now. Need a replay button and update scores state

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ export default function App() {
   return (
     <div className="app">
       <Header />
-      {/* <Replay /> */}
       <GridContainer />
     </div>
   );
@@ -17,184 +16,128 @@ function Header() {
   return <header className="header">XØX</header>;
 }
 
+// Grid container
 function GridContainer() {
-  // Each cell has different num, so i can try to check win and draw possibility
-  const nums = Array.from({ length: 9 }, (_, i) => i + 1);
-  const [player, setPlayer] = useState("X");
-  const [winner, setWinner] = useState(null);
+  // [null, null, null, null, null, null, null, null, null]
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [player, setPlayer] = useState(true);
 
-  function controlWinner() {
-    const [...cells] = document.querySelectorAll(".grid-cell");
+  // To render buttons 9 time
+  // [0, 1, 2, 3, 4, 5, 6, 7, 8]
+  const nums = Array.from({ length: 9 }, (_, i) => i);
 
-    if (winner) return;
-    // Row Conditions "X"
-    console.log(cells[0]?.textContent);
-    const conditionRowX1 =
-      cells[0]?.textContent === "X" &&
-      cells[1]?.textContent === "X" &&
-      cells[2]?.textContent === "X";
-    const conditionRowX2 =
-      cells[3]?.textContent === "X" &&
-      cells[4]?.textContent === "X" &&
-      cells[5]?.textContent === "X";
-    const conditionRowX3 =
-      cells[6]?.textContent === "X" &&
-      cells[7]?.textContent === "X" &&
-      cells[8]?.textContent === "X";
-    // Column Conditions "X"
-    const conditionColumnX1 =
-      cells[0]?.textContent === "X" &&
-      cells[3]?.textContent === "X" &&
-      cells[6]?.textContent === "X";
-    const conditionColumnX2 =
-      cells[1]?.textContent === "X" &&
-      cells[4]?.textContent === "X" &&
-      cells[7]?.textContent === "X";
-    const conditionColumnX3 =
-      cells[2]?.textContent === "X" &&
-      cells[5]?.textContent === "X" &&
-      cells[8]?.textContent === "X";
-    // Cross Conditions "X"
-    const conditionCrossX1 =
-      cells[0]?.textContent === "X" &&
-      cells[4]?.textContent === "X" &&
-      cells[8]?.textContent === "X";
-    const conditionCrossX2 =
-      cells[2]?.textContent === "X" &&
-      cells[4]?.textContent === "X" &&
-      cells[6]?.textContent === "X";
-    ///////////////////////////////////////////////////////////////
-    // Row Conditions "X"
-    const conditionRowO1 =
-      cells[0]?.textContent === "O" &&
-      cells[1]?.textContent === "O" &&
-      cells[2]?.textContent === "O";
-    const conditionRowO2 =
-      cells[3]?.textContent === "O" &&
-      cells[4]?.textContent === "O" &&
-      cells[5]?.textContent === "O";
-    const conditionRowO3 =
-      cells[6]?.textContent === "O" &&
-      cells[7]?.textContent === "O" &&
-      cells[8]?.textContent === "O";
-    // Column Conditions "O"
-    const conditionColumnO1 =
-      cells[0]?.textContent === "O" &&
-      cells[3]?.textContent === "O" &&
-      cells[6]?.textContent === "O";
-    const conditionColumnO2 =
-      cells[1]?.textContent === "O" &&
-      cells[4]?.textContent === "O" &&
-      cells[7]?.textContent === "O";
-    const conditionColumnO3 =
-      cells[2]?.textContent === "O" &&
-      cells[5]?.textContent === "O" &&
-      cells[8]?.textContent === "O";
-    // Cross Conditions "O"
-    const conditionCrossO1 =
-      cells[0]?.textContent === "O" &&
-      cells[4]?.textContent === "O" &&
-      cells[8]?.textContent === "O";
-    const conditionCrossO2 =
-      cells[2]?.textContent === "O" &&
-      cells[4]?.textContent === "O" &&
-      cells[6]?.textContent === "O";
-    if (
-      conditionRowX1 ||
-      conditionRowX2 ||
-      conditionRowX3 ||
-      conditionColumnX1 ||
-      conditionColumnX2 ||
-      conditionColumnX3 ||
-      conditionCrossX1 ||
-      conditionCrossX2
-    ) {
-      setWinner("X");
+  // Clicked button dedect with num prop
+  function handleClick(i) {
+    // If already clicked or a winner, do nothing.
+    if (squares[i] || calculateWinner(squares)) return;
+    // Copying squares
+    const nextSquares = squares.slice();
+    if (player) {
+      // value={squares[num]} ->
+      nextSquares[i] = "X";
+    } else {
+      nextSquares[i] = "O";
     }
-    if (
-      conditionRowO1 ||
-      conditionRowO2 ||
-      conditionRowO3 ||
-      conditionColumnO1 ||
-      conditionColumnO2 ||
-      conditionColumnO3 ||
-      conditionCrossO1 ||
-      conditionCrossO2
-    ) {
-      setWinner("O");
+    // Each time creates new copied array
+    setSquares(nextSquares);
+
+    // Set player to opposite
+    setPlayer(!player);
+  }
+
+  // Calculate winner by check lines.
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      // To check each line at row column or cross
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        console.log(a, b, c);
+        // Return X or O by equality.
+        return squares[a];
+      }
+    }
+    // Otherwise return null to continue playing.
+    return null;
+  }
+
+  // The result is null or "X" || "O"
+  const winner = calculateWinner(squares);
+  let status;
+  const draw = squares.every((e) => e !== null);
+  if (winner) {
+    // If any winner
+    status = "Winner " + winner;
+  } else {
+    if (draw) status = "Draw";
+    // Else change player
+    else {
+      status = player ? "X" : "O";
     }
   }
-  controlWinner();
 
   return (
     <div className="game-container">
       <div className="grid-container">
+        {/* Creates 9 buttons also "values" equals "nums" */}
         {nums.map((num) => {
-          // Then set props for child component to change content and player on each click.
           return (
             <GridCells
-              winner={winner}
-              onSetPlayer={setPlayer}
-              player={player}
+              onSquareClick={() => handleClick(num)}
+              value={squares[num]}
               key={num}
-              num={num}
             />
           );
         })}
       </div>
-      <Players winner={winner} player={player} />
+      {/* Change the content */}
+      <Players status={status} player={player} />
     </div>
   );
 }
 
-function GridCells({ player, num, onSetPlayer, winner }) {
-  const [content, setContent] = useState("X");
-  //! Başlangıçta "" değeri verdiğim için ilk tıklamayı saymıyor. Çözmen lazım
-
-  // Set content into button and change player
-  function setContents() {
-    if (content || winner) return;
-
-    // Set content
-    setContent(player);
-
-    // Change player after click
-    onSetPlayer(player === "X" ? "O" : "X");
-
-    // Draw clause
-    if (!winner && checkDraw()) {
-      console.log("Berabere");
-    }
-  }
-
-  // Draw
-  function checkDraw() {
-    const [...cells] = document.querySelectorAll(".grid-cell");
-    return cells.every((cell) => cell.innerText);
-  }
-
+// Buttons
+function GridCells({ value, onSquareClick }) {
   return (
-    <button num={num} onClick={setContents} className="grid-cell">
-      {content}
+    <button
+      onClick={onSquareClick}
+      className={`grid-cell ${value ? "grid-cell-blocked" : ""}`}
+    >
+      {value}
     </button>
   );
 }
 
 // Container holds player names and scores.
-function Players({ player, winner }) {
+function Players({ status }) {
   return (
     <div className="players-container">
       <div className="players player--1">
-        <h1 className={`player-name ${player === "X" ? "active" : ""}`}>
+        <h1 className={`player-name ${status === "X" ? "active" : ""}`}>
           Player X
         </h1>
         <span className="player-score">Score: </span>
       </div>
       <div className="players">
-        Player <span className="player-turn">{winner}</span> is winner!
+        <span className="player-turn">
+          {status === "X" || status === "O" ? status + "'s turn" : status}
+        </span>
       </div>
       <div className="players player--2">
-        <h1 className={`player-name ${player === "O" ? "active" : ""}`}>
+        <h1 className={`player-name ${status === "O" ? "active" : ""}`}>
           Player O
         </h1>
         <span className="player-score">Score: </span>
